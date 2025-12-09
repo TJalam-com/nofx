d10fcff5dbf9a8b6ee56cc7701480fc6f0930004
TJalam-com <tabrejalam786007@gmail.com>
2025-12-09 23:43:49 +0530

    Fix TypeScript error: Remove unused language variable from AuthLayout

--- web/src/layouts/AuthLayout.tsx
+++ web/src/layouts/AuthLayout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 import { Container } from '../components/Container'
-import { useLanguage } from '../contexts/LanguageContext'
 import { useSEO } from '../hooks/useSEO'
 
 interface AuthLayoutProps {
@@ -9,7 +8,6 @@ interface AuthLayoutProps {
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
-  const { language } = useLanguage()
   const { SEOComponent } = useSEO()
 
   return (
